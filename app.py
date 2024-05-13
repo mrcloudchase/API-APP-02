3fe751e19ad07067b7819a17a05733945d217146
mrcloudchase <mrcloudchase@gmail.com>
2024-05-13 17:47:44 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/calleditor', methods=['GET'])
+def get_endpoint_1():
+    endpoint_name = request.endpoint
+    return jsonify({"endpoint": endpoint_name})
+
+if __name__ == '__main__':
+    app.run(debug=True)
