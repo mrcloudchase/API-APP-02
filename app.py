c4a55e58488e57a3978e94fb00988e49a193127a
mrcloudchase <mrcloudchase@gmail.com>
2024-05-13 19:34:56 -0500

    Add dev branch

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/calleditor', methods=['GET'])
 def get_endpoint_1():
     endpoint_name = request.endpoint
-    return jsonify({"endpoint": endpoint_name})
+    return jsonify({"endpoint": endpoint_name, "branch": "dev"})
 
 if __name__ == '__main__':
     app.run(debug=True)
